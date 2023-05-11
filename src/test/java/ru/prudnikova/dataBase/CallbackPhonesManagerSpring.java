5463acd4af5bb81d863611ad5dc44d1ccd75ca52
Ekaterina Prudnikova <rvsn0913@mail.ru>
2023-05-11 17:26:15 +0300

    ff

---
+++ src/test/java/ru/prudnikova/dataBase/CallbackPhonesManagerSpring.java
@@ -0,0 +1,11 @@
+package ru.prudnikova.dataBase;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+public class CallbackPhonesManagerSpring {
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate(
+            DataSourceProvider.INSTANCE.getDataSource()
+    );
+
+}
